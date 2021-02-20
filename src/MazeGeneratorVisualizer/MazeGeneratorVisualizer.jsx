207f06ebea2bd8802211a3f0bc1b93fdd01f9c3e
Suhaan-Bhandary <suhaanbhandary1@gmail.com>
2021-02-20 12:33:39 +0530

    small changes in pdf export,but a bug is present when using mobile phones.

--- src/MazeGeneratorVisualizer/MazeGeneratorVisualizer.jsx
+++ src/MazeGeneratorVisualizer/MazeGeneratorVisualizer.jsx
@@ -697,7 +697,7 @@ const exportPdf = () => {
 
     // doc.addImage(imgData,imgPositionX,imgPositionY,width,height);
     const imgHeight = (canvas.height * 205) / canvas.width;
-    doc.text("Maze Generator - Suhaan", 98, 10);
+    doc.text("Maze Generator - Suhaan", 50, 10);
 
     doc.addImage(imgData, 2, 12, 205, imgHeight);
     doc.save("maze.pdf");
